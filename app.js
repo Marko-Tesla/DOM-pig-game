a7a069ed6b99af4ac6116f0053c542a98a9f952e
Marko-tesla <prokudinmarat@gmail.com>
2020-06-11 00:13:19 +0600

    secondDiceAdded

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ GAME RULES:
 - The first player to reach 50 points on GLOBAL score wins the game
 
 */
-var scores, roundScore, activePlayer, gamePlaying;
+var scores, roundScore, activePlayer, gamePlaying, previousRoll;
 var winScore = "50";
 
 function setWinningScore() {
@@ -25,7 +25,8 @@ gamePlaying = true;
 scores = [0, 0] ;
 roundScore = 0;
 activePlayer = 0;
-document.querySelector('.dice').style.display = 'none';
+document.querySelector('.dice-one').style.display = 'none';
+document.querySelector('.dice-two').style.display = 'none';
 document.getElementById('score-0').textContent = 0;
 document.getElementById('score-1').textContent = 0;
 document.getElementById('current-0').textContent = 0;
@@ -42,7 +43,6 @@ document.querySelector('.player-0-panel').classList.add('active');
 init();
 
 function nextPlayer() {
-
    //next player
    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
    roundScore = 0;
@@ -50,7 +50,7 @@ function nextPlayer() {
    document.getElementById('current-1').textContent = '0';
    document.querySelector('.player-0-panel').classList.toggle('active');
    document.querySelector('.player-1-panel').classList.toggle('active');
-   document.querySelector('.dice').style.display = 'none';
+  
 }
 
 // "Roll" button
@@ -58,18 +58,22 @@ function nextPlayer() {
 document.querySelector('.btn-roll').addEventListener('click', function(){
    if(gamePlaying) {
       // 1. Random number
-      dice = Math.floor(Math.random() * 6) + 1;
+      diceOne = Math.floor(Math.random() * 6) + 1;
+      diceTwo = Math.floor(Math.random() * 6) + 1;
       // 2. Display the result
-      var diceDOM = document.querySelector('.dice');
-      diceDOM.style.display = 'block';
-      diceDOM.src = 'img/dice-'+ dice +'.png';
+      var diceDOMone = document.querySelector('.dice-one');
+      var diceDOMtwo = document.querySelector('.dice-two');
+      diceDOMone.style.display = 'block';
+      diceDOMone.src = 'img/dice-'+ diceOne +'.png';
+      diceDOMtwo.style.display = 'block';
+      diceDOMtwo.src = 'img/dice-'+ diceTwo +'.png';
       // 3. Update the round score IF the rolled number was not a 1
-      if (dice !== 1) {
-         //add score
-         roundScore += dice;
-         document.querySelector('#current-' + activePlayer).textContent = roundScore;
-      } else {
-         nextPlayer();
+         if (diceOne !== 1 && diceTwo !==1) {
+            //add score
+            roundScore += (diceOne+diceTwo);
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+         }  else {
+            nextPlayer();
       }
    }
 });
@@ -85,7 +89,8 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
       // check if player won the game
       if (scores[activePlayer] >= winScore) {
          document.querySelector('#name-' +  activePlayer).textContent = 'Winner!';
-         document.querySelector('.dice').style.display = 'none'; // better to create an 'active' class
+         document.querySelector('.dice-one').style.display = 'none'; // better to create an 'active' class
+         document.querySelector('.dice-two').style.display = 'none';
          document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
          document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
          gamePlaying = false;
